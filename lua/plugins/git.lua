991f06cb761092b41041456f80b8cb2106c40172
ResiDev <alexanderboitier@gmail.com>
2025-07-24 23:18:49 +0100

    tried to add debugging for nodejs, coudln't get it working for ts. added lazygit, and better git keybinds

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,26 @@
 -- ~/.config/nvim/lua/plugins/git.lua
 return {
   { 'akinsho/git-conflict.nvim', version = '*', config = true },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
 
   {
     'lewis6991/gitsigns.nvim',
@@ -35,8 +55,8 @@ return {
         end, { expr = true, desc = 'Previous hunk' })
 
         -- Actions
-        map('n', '<leader>gd', gs.diffthis, { desc = 'Diff against the index' })
-        map('n', '<leader>gD', function() gs.diffthis('~') end, { desc = 'Diff against last commit' })
+        map('n', '<leader>gd', function() gs.diffthis('~') end, { desc = 'Diff against last commit' })
+        map('n', '<leader>gD', gs.diffthis, { desc = 'Diff against the index' })
         map('n', '<leader>ga', function() gs.diffthis('@') end, { desc = 'Diff against staged' })
       end,
     },
