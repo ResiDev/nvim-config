b5fa5aacb467b5ba3cdf5867f9cdfeff8fbcd8db
alexboitier <alexanderboitier@gmail.com>
2025-11-30 13:36:14 +0000

    feat: arrow and marks no multi cursor atm

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -47,118 +47,155 @@ return {
     },
   },
   {
-    'cbochs/grapple.nvim', -- For quick file navigation
+    'otavioschwanck/arrow.nvim',
     dependencies = {
-      'nvim-tree/nvim-web-devicons',
+      { 'nvim-tree/nvim-web-devicons' },
     },
     opts = {
-      icons = true, -- setting to "true" requires "nvim-web-devicons"
-      status = false,
-      storage = 'json',
+      show_icons = true,
+      leader_key = 'e', -- Recommended to be a single key
     },
     keys = {
-      { '<leader>a', '<cmd>Grapple toggle<cr>', desc = 'Tag a file' },
-      { '<C-e>', '<cmd>Grapple toggle_tags<cr>', desc = 'Toggle tags menu' },
-
-      { '<leader>1', '<cmd>Grapple select index=1<cr>', desc = 'Select first tag' },
-      { '<leader>2', '<cmd>Grapple select index=2<cr>', desc = 'Select second tag' },
-      { '<leader>3', '<cmd>Grapple select index=3<cr>', desc = 'Select third tag' },
-      { '<leader>4', '<cmd>Grapple select index=4<cr>', desc = 'Select fourth tag' },
-
-      { '<C-l>', '<cmd>Grapple cycle_tags next<cr>', desc = 'Go to next tag' },
-      { '<C-h>', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Go to previous tag' },
+      {
+        '<C-e>',
+        function()
+          require('arrow.ui').openMenu()
+        end,
+        desc = 'Open arrow menu',
+      },
+      {
+        '<C-l>',
+        function()
+          require('arrow.persist').next()
+        end,
+        desc = 'Next arrow mark',
+      },
+      {
+        '<C-h>',
+        function()
+          require('arrow.persist').previous()
+        end,
+        desc = 'Previous arrow mark',
+      },
+      {
+        '<leader>a',
+        function()
+          require('arrow.persist').toggle()
+        end,
+        desc = 'Toggle arrow mark',
+      },
     },
   },
-
   {
-    'jake-stewart/multicursor.nvim',
-    branch = '1.0',
+    'chentoast/marks.nvim',
+    event = 'VeryLazy',
+    opts = {},
+  },
+  {
+    'fnune/recall.nvim',
+    version = '*',
     config = function()
-      local mc = require 'multicursor-nvim'
+      local recall = require 'recall'
 
-      mc.setup()
+      recall.setup {}
 
-      local set = vim.keymap.set
-
-      -- Add or skip cursor above/below the main cursor.
-      set({ 'n', 'v' }, '<leader>mk', function()
-        mc.lineAddCursor(-1)
-      end, { desc = 'Add cursor one line above' })
-      set({ 'n', 'v' }, '<leader>mj', function()
-        mc.lineAddCursor(1)
-      end, { desc = 'Add cursor one line below' })
-      set({ 'n', 'v' }, '<leader>mK', function()
-        mc.lineSkipCursor(-1)
-      end, { desc = 'Skip adding cursor one line above' })
-      set({ 'n', 'v' }, '<leader>mJ', function()
-        mc.lineSkipCursor(1)
-      end, { desc = 'Skip adding cursor one line below' })
-
-      -- Rotate between cursors
-      set({ 'n', 'v' }, '<leader>mh', mc.nextCursor, { desc = 'Move to next cursor' })
-      set({ 'n', 'v' }, '<leader>ml', mc.prevCursor, { desc = 'Move to previous cursor' })
-      -- Mouse control
-      set('n', '<c-leftmouse>', mc.handleMouse, { desc = 'Add/remove cursor at mouse click' })
-      -- Toggle cursor at current position
-      set({ 'n', 'v' }, '<c-q>', mc.toggleCursor, { desc = 'Toggle cursor at current position' })
-
-      -- Add or skip adding a new cursor by matching word/selection
-      set({ 'n', 'v' }, '<leader>mn', function()
-        mc.matchAddCursor(1)
-      end, { desc = 'MCursor: Add at next match' })
-      set({ 'n', 'v' }, '<leader>mN', function()
-        mc.matchAddCursor(-1)
-      end, { desc = 'MCursor: Add at previous match' })
-      set({ 'n', 'v' }, '<leader>ms', function()
-        mc.matchSkipCursor(1)
-      end, { desc = 'MCursor: Skip next match' })
-      set({ 'n', 'v' }, '<leader>mS', function()
-        mc.matchSkipCursor(-1)
-      end, { desc = 'MCursor: Skip previous match' })
-      set({ 'n', 'v' }, '<leader>ma', mc.matchAllAddCursors, { desc = 'MCursor: Add all matches' })
-      -- Delete current cursor
-      set({ 'n', 'v' }, '<leader>md', mc.deleteCursor, { desc = 'Delete current cursor' })
-      -- bring back cursors if you accidentally clear them
-      set('n', '<leader>mu', mc.restoreCursors, { desc = 'Undo cursor deletion' })
-      -- Align cursor columns.
-      set('v', '<leader>ma', mc.alignCursors, { desc = 'Align cursor columns' })
-
-      -- ESC handling for cursors
-      set('n', '<esc>', function()
-        if not mc.cursorsEnabled() then
-          mc.enableCursors()
-        elseif mc.hasCursors() then
-          mc.clearCursors()
-        else
-          -- Default <esc> handler.
-        end
-      end, { desc = 'Enable cursors, clear if active, or normal ESC' })
-
-      -- Visual mode operations
-      set('v', 'S', mc.splitCursors, { desc = 'Split selection into cursors at regex matches' })
-      set('v', 'I', mc.insertVisual, { desc = 'Insert at start of each selected line' })
-      set('v', 'A', mc.appendVisual, { desc = 'Append at end of each selected line' })
-      set('v', 'M', mc.matchCursors, { desc = 'Add cursors at regex matches in selection' })
-
-      -- Rotate/transpose operations
-      set('v', '<leader>mt', function()
-        mc.transposeCursors(1)
-      end, { desc = 'Rotate selected texts forward' })
-
-      set('v', '<leader>mT', function()
-        mc.transposeCursors(-1)
-      end, { desc = 'Rotate selected texts backward' })
-
-      -- Customize how cursors look.
-      local hl = vim.api.nvim_set_hl
-      hl(0, 'MultiCursorCursor', { link = 'Cursor' })
-      hl(0, 'MultiCursorVisual', { link = 'Visual' })
-      hl(0, 'MultiCursorSign', { link = 'SignColumn' })
-      hl(0, 'MultiCursorDisabledCursor', { link = 'Visual' })
-      hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
-      hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
+      vim.keymap.set('n', '<leader>mm', recall.toggle, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>mn', recall.goto_next, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>mp', recall.goto_prev, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>mc', recall.clear, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>ml', require('recall.snacks').pick, { noremap = true, silent = true })
     end,
   },
+
+  -- {
+  --   'jake-stewart/multicursor.nvim',
+  --   branch = '1.0',
+  --   config = function()
+  --     local mc = require 'multicursor-nvim'
+  --
+  --     mc.setup()
+  --
+  --     local set = vim.keymap.set
+  --
+  --     -- Add or skip cursor above/below the main cursor.
+  --     set({ 'n', 'v' }, '<leader>mk', function()
+  --       mc.lineAddCursor(-1)
+  --     end, { desc = 'Add cursor one line above' })
+  --     set({ 'n', 'v' }, '<leader>mj', function()
+  --       mc.lineAddCursor(1)
+  --     end, { desc = 'Add cursor one line below' })
+  --     set({ 'n', 'v' }, '<leader>mK', function()
+  --       mc.lineSkipCursor(-1)
+  --     end, { desc = 'Skip adding cursor one line above' })
+  --     set({ 'n', 'v' }, '<leader>mJ', function()
+  --       mc.lineSkipCursor(1)
+  --     end, { desc = 'Skip adding cursor one line below' })
+  --
+  --     -- Rotate between cursors
+  --     set({ 'n', 'v' }, '<leader>mh', mc.nextCursor, { desc = 'Move to next cursor' })
+  --     set({ 'n', 'v' }, '<leader>ml', mc.prevCursor, { desc = 'Move to previous cursor' })
+  --     -- Mouse control
+  --     set('n', '<c-leftmouse>', mc.handleMouse, { desc = 'Add/remove cursor at mouse click' })
+  --     -- Toggle cursor at current position
+  --     set({ 'n', 'v' }, '<c-q>', mc.toggleCursor, { desc = 'Toggle cursor at current position' })
+  --
+  --     -- Add or skip adding a new cursor by matching word/selection
+  --     set({ 'n', 'v' }, '<leader>mn', function()
+  --       mc.matchAddCursor(1)
+  --     end, { desc = 'MCursor: Add at next match' })
+  --     set({ 'n', 'v' }, '<leader>mN', function()
+  --       mc.matchAddCursor(-1)
+  --     end, { desc = 'MCursor: Add at previous match' })
+  --     set({ 'n', 'v' }, '<leader>ms', function()
+  --       mc.matchSkipCursor(1)
+  --     end, { desc = 'MCursor: Skip next match' })
+  --     set({ 'n', 'v' }, '<leader>mS', function()
+  --       mc.matchSkipCursor(-1)
+  --     end, { desc = 'MCursor: Skip previous match' })
+  --     set({ 'n', 'v' }, '<leader>ma', mc.matchAllAddCursors, { desc = 'MCursor: Add all matches' })
+  --     -- Delete current cursor
+  --     set({ 'n', 'v' }, '<leader>md', mc.deleteCursor, { desc = 'Delete current cursor' })
+  --     -- bring back cursors if you accidentally clear them
+  --     set('n', '<leader>mu', mc.restoreCursors, { desc = 'Undo cursor deletion' })
+  --     -- Align cursor columns.
+  --     set('v', '<leader>ma', mc.alignCursors, { desc = 'Align cursor columns' })
+  --
+  --     -- ESC handling for cursors
+  --     set('n', '<esc>', function()
+  --       if not mc.cursorsEnabled() then
+  --         mc.enableCursors()
+  --       elseif mc.hasCursors() then
+  --         mc.clearCursors()
+  --       else
+  --         -- Default <esc> handler.
+  --       end
+  --     end, { desc = 'Enable cursors, clear if active, or normal ESC' })
+  --
+  --     -- Visual mode operations
+  --     set('v', 'S', mc.splitCursors, { desc = 'Split selection into cursors at regex matches' })
+  --     set('v', 'I', mc.insertVisual, { desc = 'Insert at start of each selected line' })
+  --     set('v', 'A', mc.appendVisual, { desc = 'Append at end of each selected line' })
+  --     set('v', 'M', mc.matchCursors, { desc = 'Add cursors at regex matches in selection' })
+  --
+  --     -- Rotate/transpose operations
+  --     set('v', '<leader>mt', function()
+  --       mc.transposeCursors(1)
+  --     end, { desc = 'Rotate selected texts forward' })
+  --
+  --     set('v', '<leader>mT', function()
+  --       mc.transposeCursors(-1)
+  --     end, { desc = 'Rotate selected texts backward' })
+  --
+  --     -- Customize how cursors look.
+  --     local hl = vim.api.nvim_set_hl
+  --     hl(0, 'MultiCursorCursor', { link = 'Cursor' })
+  --     hl(0, 'MultiCursorVisual', { link = 'Visual' })
+  --     hl(0, 'MultiCursorSign', { link = 'SignColumn' })
+  --     hl(0, 'MultiCursorDisabledCursor', { link = 'Visual' })
+  --     hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
+  --     hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
+  --   end,
+  -- },
 
   {
     'mikavilpas/yazi.nvim',
